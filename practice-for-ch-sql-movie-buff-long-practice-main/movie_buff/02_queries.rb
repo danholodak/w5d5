054c336eb4d3704d3af2e73338a114980a2f5bef
Dan Holodak <danholodak@gmail.com>
2023-02-10 15:46:46 -0500

    did part 2

--- practice-for-ch-sql-movie-buff-long-practice-main/movie_buff/02_queries.rb
+++ practice-for-ch-sql-movie-buff-long-practice-main/movie_buff/02_queries.rb
@@ -1,18 +1,27 @@
 def eighties_b_movies
   # List all the movies from 1980-1989 with scores falling between 3 and 5
   # (inclusive). Show the id, title, year, and score.
-  
+  Movie
+    .select("id, title, yr, score")
+    .where("yr BETWEEN 1980 AND 1989 AND score BETWEEN 3 AND 5")
 end
 
 def bad_years
   # List the years in which no movie with a rating above 8 was released.
-  
+  Movie
+    # .select(:yr)
+    .where("yr NOT IN (:good_years)", good_years: Movie.select(:yr)
+    .group(:yr)
+    .where("score > 8")).distinct.pluck(:yr)
 end
 
 def cast_list(title)
   # List all the actors for a particular movie, given the title.
   # Sort the results by starring order (ord). Show the actor id and name.
-  
+  Actor
+    .select("actors.id, actors.name")
+    .joins(:movies).where("movies.title = (?)", title)
+    .order("castings.ord")
 end
 
 def vanity_projects
@@ -20,7 +29,11 @@ def vanity_projects
   # starring actor. Show the movie id, title, and director's name.
 
   # Note: Directors appear in the 'actors' table.
-  
+  Movie
+    .joins(:director)
+    .select("movies.id, movies.title, actors.name")
+    .joins("castings on casting.")
+    .
 end
 
 def most_supportive
