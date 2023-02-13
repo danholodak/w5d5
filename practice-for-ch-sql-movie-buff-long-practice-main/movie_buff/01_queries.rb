352d26ca300db968220aa6e2c22c3215c946157b
Fahim Khan <fk652@nyu.edu>
2023-02-13 17:20:53 -0500

    update: 02_queries

--- practice-for-ch-sql-movie-buff-long-practice-main/movie_buff/01_queries.rb
+++ practice-for-ch-sql-movie-buff-long-practice-main/movie_buff/01_queries.rb
@@ -6,7 +6,9 @@ def it_was_ok
   # You can use ranges (a..b) inside a where method.
   #
   # Find the id, title, and score of all movies with scores between 2 and 3.
-  Movie.select("id, title, score").where("score BETWEEN 2 AND 3")
+  Movie
+    .select("id, title, score")
+    .where("score BETWEEN 2 AND 3")
 end
 
 def harrison_ford
@@ -79,5 +81,7 @@ def movie_names_before_1940
   # improve performance for larger queries.
   #
   # Use pluck to find the title of all movies made before 1940.
-  Movie.where("yr < 1940").pluck('title')
+  Movie
+    .where("yr < 1940")
+    .pluck('title')
 end
